ee69d4ebd89d0a2eacb99c7010d4ae28e6bfead0
Mineev Buryat <mineev.buryat@gmail.com>
2024-12-18 12:27:51 +0800

    del file

--- mysite/main/admin.py
+++ mysite/main/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import Menu
+from .models import Menu, UsefulLink
 # Register your models here.
 
 @admin.register(Menu)
 class MenuAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(UsefulLink)
+class UsefulLinkAdmin(admin.ModelAdmin):
     pass
--- mysite/main/models.py
+++ mysite/main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+# from django.date
 
 # Create your models here.
 class Menu(models.Model):
@@ -13,3 +14,21 @@ class Menu(models.Model):
         ordering = ('position',)
         verbose_name = 'Пункт меню'
         verbose_name_plural = 'Пункты меню'
+
+class UsefulLink(models.Model):
+    name = models.CharField(max_length=60)
+    url = models.URLField()
+    description = models.CharField(max_length=300,
+                                   blank=True)
+    added = models.DateTimeField(auto_now_add=True)
+    usefulness = models.IntegerField(default=0)  # Поле полезности
+
+    class Meta:
+        verbose_name = 'Полезная ссылка'
+        verbose_name_plural = 'Полезные ссылки'
+
+    # def 
+
+    def __str__(self):
+        return f"{self.name} ({self.url})"
+    
