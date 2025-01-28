33ad5a16ec89fd959f22c9c4f1bbb62b9b7b46ec
Mineev <mineev.buryat@gmail.com>
2025-01-28 11:38:11 +0800

    sqlcreatedb

---
+++ dbcreate.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE mysite;
+CREATE USER django WITH PASSWORD '34652817';
+ALTER ROLE django SET client_encoding TO 'utf8';
+ALTER ROLE django SET default_transaction_isolation TO 'read committed';
+ALTER ROLE django SET timezone TO 'UTC';
+GRANT ALL PRIVILEGES ON DATABASE mysite TO django;
