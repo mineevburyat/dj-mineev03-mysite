9bbb70f57d994b34cb8c6ee1a868946cceb2240b
Mineev <mineev.buryat@gmail.com>
2025-12-22 14:05:00 +0800

    приложение главной страницы

---
+++ home/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from .views import HomePage
+
+
+app_name = 'home'
+
+urlpatterns = [
+    path('', HomePage.as_view(), name='home'),
+]
---
+++ home/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import View, DetailView
+
+
+class HomePage(View):
+    template_name = 'home/index.html'
+    context = {
+        'title': 'Главная',
+        'work_years': '8',
+        'email': 'alex@mineev-03.ru',
+        'phone': '+7 (924) 5555 937'}
+    def get(self, request):
+        return render(request, template_name=self.template_name, context=self.context)
